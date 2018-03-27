c2232d2455d78fadddebbb8e87c37e4f2d5f5168
Andrea Tosato <andrea.tosato@hotmail.it>
2018-03-27 22:05:43 +0200

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,28 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Startup");
+            var log = new LoggerConfiguration()
+                .WriteTo.RollingFile("log-{Date}.txt")
+                .CreateLogger();
+
+            while(true)
+            {
+                log.Information(DateTime.Now.ToLongDateString());
+                Task.Delay(2000);
+            }
+
+            Console.Read();
+        }
+    }
+}
